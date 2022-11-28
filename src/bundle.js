001ea7f6751baf6786f0a83d127e46afd62aa874
Heri Kaniugu <hekan12@gmail.com>
2022-11-28 14:51:15 +0300

    added multiplication

--- src/bundle.js
+++ src/bundle.js
@@ -1,10 +1,14 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const sum = require("./sum");
+const mul = require("./mul");
+
+console.log('test');
 
 const answerElement = document.getElementById("answer");
 const inputElement = document.getElementById("input");
 const numElements = Array.from(document.querySelectorAll(".number"));
 const operatorBtn = Array.from(document.querySelectorAll(".operator"));
+
 let firstValue;
 
 numElements.forEach((btn) => {
@@ -32,15 +36,35 @@ operatorBtn.forEach((btn) => {
 			}
 
 			inputElement.append("+");
-		} else if (operator === "equal") {
-			const values = inputValue.split("+");
-			console.log("values", values);
+		}
 
-			const _sum = values.reduce((lastSum, value) => {
-				return sum(parseInt(lastSum), parseInt(value));
-			});
-			answerElement.innerText = _sum ?? 0;
-		} else if (operator === "clear") {
+		else if (operator === "multiply") {
+			if (inputValue[0] === "x" || inputValue.at(-1) === "x") {
+				return;
+			}
+
+			inputElement.append("x");
+		}
+		
+		else if (operator === "equal") {
+			let ans;
+			if (inputValue.includes("+")) {
+				const values = inputValue.split("+");
+				console.log("values", values);
+				ans = values.reduce((lastSum, value) => {
+					return sum(parseInt(lastSum), parseInt(value));
+				});
+			} else if (inputValue.includes("x")) {
+				const values = inputValue.split("x");
+				console.log(values);
+				ans = values.reduce((lastSum, value) => {
+					return mul(parseInt(lastSum), parseInt(value));
+				});
+			}
+			answerElement.innerText = ans ?? 0;
+		}
+		
+		else if (operator === "clear") {
 			inputElement.innerText = "0";
 			answerElement.innerText = "0";
 		} else if (operator === "delete") {
@@ -52,12 +76,23 @@ operatorBtn.forEach((btn) => {
 	});
 });
 
-},{"./sum":2}],2:[function(require,module,exports){
+},{"./mul":2,"./sum":3}],2:[function(require,module,exports){
+const mul = (a, b) => {
+	if (typeof a != "number" || typeof b != "number") {
+		throw `invalid data type`;
+	}
+	return a * b;
+};
+
+module.exports = mul;
+
+// console.log('test2');
+},{}],3:[function(require,module,exports){
 const sum = (a, b) => {
 	if (typeof a != "number" || typeof b != "number") {
 		throw `invalid data type`;
 	}
-	return a + b;
+	return a+b;
 };
 
 module.exports = sum;
